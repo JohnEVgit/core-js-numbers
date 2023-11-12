6caf49ba58ee2043d37849521d9916f7b6651846
Yauheni Paloika <john13ev@gmail.com>
2023-11-13 02:57:27 +0400

    feat: add getLinearEquationRoot function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -77,9 +77,7 @@ const getDistanceBetweenPoints = (x1, y1, x2, y2) =>
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
-}
+const getLinearEquationRoot = (a, b) => -b / a;
 
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
