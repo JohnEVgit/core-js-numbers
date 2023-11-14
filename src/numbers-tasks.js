30fe1403b40660baabe65d894a8326e0b144233b
Yauheni Paloika <john13ev@gmail.com>
2023-11-15 00:09:13 +0400

    feat: add getSumOfNumbers function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -560,9 +560,8 @@ const getIntegerPartNumber = (number) => {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
-}
+const getSumOfNumbers = (...args) =>
+  args.reduce((acc, current) => acc + current, 0).toFixed(10);
 
 /**
  * Returns the largest number.
